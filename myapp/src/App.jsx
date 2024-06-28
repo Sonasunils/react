4d38a545210812e2f5ce6f69b1787c69d8325cc9
Sonasunils <sona2002sunil@gmail.com>
2024-06-28 16:54:14 +0530

    Create react app
    Add functionality to add predefined data entries to the table one by one on button click

---
+++ myapp/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import MyButton  from "./new";
+
+
+
+function App()
+{
+  
+  return(
+  <> <div><MyButton/></div>
+  <div><h1 className='new'>Welcome to my page</h1></div>
+  <img className="avatar" />
+  </>)
+}
+export default App
